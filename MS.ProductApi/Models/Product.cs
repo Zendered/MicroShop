7367e14c4e6f790075c3c284a57683a07b68be75
zend <luizz.ggr@gmail.com>
2022-10-25 18:52:19 -0300

    category and product relation

--- MS.ProductApi/Models/Product.cs
+++ MS.ProductApi/Models/Product.cs
@@ -8,4 +8,6 @@ public class Product
     public string? Description { get; set; }
     public long Stock { get; set; }
     public string? ImageUrl { get; set; }
+    public Category? Category { get; set; }
+    public int CategoryId { get; set; }
 }
