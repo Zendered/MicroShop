b9afdc928d9d0419d0b64fc56a52828dcd90da55
zend <luizz.ggr@gmail.com>
2022-10-25 19:04:16 -0300

    db config

---
+++ MS.ProductApi/Context/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MS.ProductApi.Models;
+
+namespace MS.ProductApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt) { }
+    }
+}
