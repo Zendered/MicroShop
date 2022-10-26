20045b0e0311a24e5e91676e4fb1dc096793ccf7
zend <luizz.ggr@gmail.com>
2022-10-26 17:19:26 -0300

    creating category DTO

---
+++ MS.ProductApi/DTOs/Categories/CategoryDTO.cs
@@ -0,0 +1,14 @@
+﻿using MS.ProductApi.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace MS.ProductApi.DTOs.Categories;
+
+public class CategoryDTO
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "The Name is Required")]
+    [MinLength(3)]
+    [MaxLength(100)]
+    public string? Name { get; set; }
+    public ICollection<Product>? Products { get; set; }
+}
