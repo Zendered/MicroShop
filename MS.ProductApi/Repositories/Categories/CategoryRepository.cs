7f0e110907b717f1067dc3a279e45290e0bf827f
zend <luizz.ggr@gmail.com>
2022-10-26 18:10:41 -0300

    category repository

---
+++ MS.ProductApi/Repositories/Categories/CategoryRepository.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using MS.ProductApi.Context;
+using MS.ProductApi.Models;
+
+namespace MS.ProductApi.Repositories.Categories;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly AppDbContext ctx;
+    private readonly IMapper mapper;
+
+    public CategoryRepository(AppDbContext ctx, IMapper mapper)
+    {
+        this.ctx = ctx;
+        this.mapper = mapper;
+    }
+
+    public async Task<Category> Create(Category category)
+    {
+        ctx.Add(category);
+        await ctx.SaveChangesAsync();
+        return category;
+    }
+    public async Task<IEnumerable<Category>> GetAll()
+    {
+        var categories = await ctx.Categories.ToListAsync();
+        return categories;
+    }
+
+    public async Task<Category> GetById(int id)
+    {
+        var category = await ctx.Categories.FindAsync(id);
+        return category;
+    }
+    public async Task<IEnumerable<Category>> GetCategoriesProucts()
+    {
+        var category = await ctx.Categories
+            .Include(c => c.Products)
+            .ToListAsync();
+        return category;
+    }
+    public async Task<Category> Update(Category category)
+    {
+        ctx.Entry(category).State = EntityState.Modified;
+        await ctx.SaveChangesAsync();
+        return category;
+    }
+
+    public async Task<Category> Delete(int id)
+    {
+        var category = await GetById(id);
+        ctx.Categories.Remove(category);
+        await ctx.SaveChangesAsync();
+        return category;
+    }
+}
