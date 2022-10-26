94cca62dc1f1d733de970e98284cc9b91ff2a54c
zend <luizz.ggr@gmail.com>
2022-10-26 18:10:31 -0300

    Interface product repository

---
+++ MS.ProductApi/Repositories/Products/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using MS.ProductApi.Models;
+
+namespace MS.ProductApi.Repositories.Products;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAll();
+    Task<Product> GetById(int id);
+    Task<Product> Create(Product category);
+    Task<Product> Update(Product category);
+    Task<Product> Delete(int id);
+}
