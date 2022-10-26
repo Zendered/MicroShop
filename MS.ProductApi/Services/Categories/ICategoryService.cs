5a04f0b3e18f4bcbe6684ca03b558a15e0d23c00
zend <luizz.ggr@gmail.com>
2022-10-26 19:48:17 -0300

    Interface category service

---
+++ MS.ProductApi/Services/Categories/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using MS.ProductApi.DTOs.Categories;
+
+namespace MS.ProductApi.Services.Categories;
+
+public interface ICategoryService
+{
+    Task AddCategory(CategoryDTO category);
+    Task<IEnumerable<CategoryDTO>> GetCategories();
+    Task<CategoryDTO> GetCategoryById(int id);
+    Task<IEnumerable<CategoryDTO>> GetCategoriesProducts();
+    Task UpdateCategory(CategoryDTO category);
+    Task DeleteCategory(int id);
+}
