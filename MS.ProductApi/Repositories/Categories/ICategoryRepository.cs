68d59a9268b8fb9b3ace2c3a6cb7da98a7d06c87
zend <luizz.ggr@gmail.com>
2022-10-26 18:10:20 -0300

    Interface category repository

---
+++ MS.ProductApi/Repositories/Categories/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using MS.ProductApi.Models;
+
+namespace MS.ProductApi.Repositories.Categories;
+
+public interface ICategoryRepository
+{
+    Task<Category> Create(Category category);
+    Task<IEnumerable<Category>> GetAll();
+    Task<Category> GetById(int id);
+    Task<IEnumerable<Category>> GetCategoriesProucts();
+    Task<Category> Update(Category category);
+    Task<Category> Delete(int id);
+}
