36287d7957a98ba188e2fd03d7b7b28880fb15fd
zend <luizz.ggr@gmail.com>
2022-10-26 19:48:27 -0300

    Interface product service

---
+++ MS.ProductApi/Services/Products/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using MS.ProductApi.DTOs.Products;
+
+namespace MS.ProductApi.Services.Products;
+
+public interface IProductService
+{
+    Task AddProduct(ProductDTO product);
+    Task<IEnumerable<ProductDTO>> GetProducts();
+    Task<ProductDTO> GetProductById(int id);
+    Task UpdateProduct(ProductDTO product);
+    Task DeleteProduct(int id);
+}
